16e522ca8e6e694752f30ecec2acdde2805b9394
thoth-ky <28805113+thoth-ky@users.noreply.github.com>
2019-07-10 16:47:29 +0300

    Setup factorybot and faker

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -1,9 +1,17 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe IndexController, type: :controller do
 
   describe "GET #index" do
+    it "returns http redirect before sign in" do
+      get :index
+      expect(response).to have_http_status(:redirect)
+    end
+
     it "returns http success" do
+      sign_in create(:user)
       get :index
       expect(response).to have_http_status(:success)
     end
